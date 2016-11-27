3e75f4ac56dd09ec81502781e80a31ea8069dd4d
Joel Cowie <joel.a.cowie@gmail.com>
2016-11-27 21:04:47 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,63 @@
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  array = []
+  data.each do |seasons, details|
+    details.each do |details2|
+      if seasons == season && details2["status"] == "Winner"
+        winner = details2["name"]
+        array = winner.split(" ")
+      end
+    end
+  end
+  array[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  array = []
+  data.each do |seasons, details|
+    details.each do |details2|
+      if details2["occupation"] == occupation
+        person = details2["name"]
+        array.push(person)
+      end
+    end
+  end
+  array[0]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |seasons, details|
+    details.each do |details2|
+      if details2["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  return counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  array = []
+  data.each do |seasons, details|
+    details.each do |details2|
+      if details2["hometown"] == hometown
+        array.push(details2["occupation"])
+      end
+    end
+  end
+array[0]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  array = []
+  data.each do |seasons, details|
+    details.each do |details2|
+      if seasons == season
+      array.push(details2["age"])
+      array = array.map { |x| x.to_f }
+    end
+    end
+  end
+return (array.inject{ |sum, y| sum + y } / array.size).round
 end
